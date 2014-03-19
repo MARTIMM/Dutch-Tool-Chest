42b8a3f17bbbcca367db4438a6b757c57e79b7f3
Marcel Timmerman <mt1957@gmail.com>
2014-03-19 16:30:20 +0100

    Generate Project.yml and README

--- buildBuilder.pl
+++ buildBuilder.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 #
 use Modern::Perl;
+use version; our $VERSION = qv('v0.0.1');
+use 5.010001;
+
 use namespace::autoclean;
 use English qw(-no_match_vars); # Avoids regex perf penalty, perl < v5.016000
 
@@ -9,6 +12,8 @@ extends qw(AppState::Ext::Constants);
 
 use AppState;
 use File::Path();
+use DateTime;
+use Software::License;
 
 #-------------------------------------------------------------------------------
 #
@@ -41,22 +46,46 @@ EODSCR
           ]
         ]
       }
-    , init_arg          => undef
-    , lazy              => 1
+    , init_arg                  => undef
+    , lazy                      => 1
     );
 
 has options =>
-    ( is                => 'ro'
-    , isa               => 'ArrayRef'
-    , default           =>
+    ( is                        => 'ro'
+    , isa                       => 'ArrayRef'
+    , default                   =>
       sub
       { return
-        [ [ 'help|h'    => 'Help on this program']
-        , [ 'verbose|v' => 'Show all tests done by prove']
+        [ [ 'help|h'            => 'Help on this program']
+        , [ 'verbose|v+'        => 'Show all tests done by prove']
         ]
       }
-    , init_arg          => undef
-    , lazy              => 1
+    , init_arg                  => undef
+    , lazy                      => 1
+    );
+
+has dependencies =>
+    ( is                        => 'rw'
+    , isa                       => 'HashRef'
+    , default                   => sub { return {}; }
+    , traits                    => ['Hash']
+    , handles                   =>
+      { get_dependency          => 'get'
+      , set_dependency          => 'set'
+      , get_dep_keys            => 'keys'
+      }
+    );
+
+has config_dependencies =>
+    ( is                        => 'rw'
+    , isa                       => 'HashRef'
+    , default                   => sub { return {}; }
+    , traits                    => ['Hash']
+    , handles                   =>
+      { get_conf_dependency     => 'get'
+      , set_conf_dependency     => 'set'
+      , get_conf_dep_keys       => 'keys'
+      }
     );
 
 #-------------------------------------------------------------------------------
@@ -77,7 +106,7 @@ sub BUILD
 #    $self->const( '',qw(M_INFO M_SUCCESS));
 
     $self->const( 'C_DISTRODIREXISTS',  qw(M_ERROR M_FORCED));
-#    $self->const( '',qw(M_ERROR M_FAIL));
+    $self->const( 'C_EVALERROR',        qw(M_ERROR M_FAIL));
 #    $self->const( '',qw(M_ERROR M_FAIL));
 #    $self->const( '',qw(M_ERROR M_FAIL));
 #    $self->const( '',qw(M_ERROR M_FAIL));
@@ -91,7 +120,12 @@ sub BUILD
 #
 my $self = main->new;
 
+#-------------------------------------------------------------------------------
+# Get AppState object. Plan is not to use a temp and work directory.
+#
 my $app = AppState->instance;
+$app->use_work_dir(0);
+$app->use_temp_dir(0);
 $app->initialize;
 $app->check_directories;
 
@@ -99,16 +133,15 @@ $app->check_directories;
 # Setup logging
 #
 my $log = $app->get_app_object('Log');
-$log->log_file('Distribution-Tests.log');
 
-#$log->show_on_error(0);
+$log->show_on_error(0);
+$log->show_on_warning(0);
 $log->do_append_log(0);
 
 $log->start_logging;
 
 #$log->do_flush_log(1);
-#  $log->log_mask($self->M_ERROR|$self->M_WARNING);
-$log->log_mask($self->M_ERROR|$self->M_WARNING|$self->M_INFO);
+$log->log_mask($self->M_SEVERITY);
 
 $log->add_tag('.BB');
 
@@ -120,40 +153,47 @@ $cmd->config_getopt_long(qw(bundling));
 
 # Defaults
 #
-$cmd->setOption( blib => 0, lib => 0, verbose => 1);
+$cmd->set_option( verbose => 0);
 
 # Initialize
 #
 $cmd->initialize( $self->description, $self->arguments, $self->options
-#m              , $self->usage, $self->examples
+#               , $self->usage, $self->examples
                 );
 #say (join ', ', map {"$_=" . $cmd->getOption($_)} sort $cmd->get_options);
 
 # Noverbose or quiet will make proving less noisy
 #
-$cmd->setOption(verbose => 0) if $cmd->getOption('quiet');
+#$cmd->set_option(verbose => 0) if $cmd->getOption('quiet');
 
-if( $log->get_last_error == $cmd->C_CMD_OPTPROCFAIL or $cmd->getOption('help'))
+if( $log->get_last_error == $cmd->C_CMD_OPTPROCFAIL or $cmd->get_option('help'))
 {
   say $cmd->usage;
   $self->leave;
 }
+
+$log->show_on_error(1) if $cmd->get_option('verbose') > 1;
+$log->show_on_warning(1) if $cmd->get_option('verbose') > 2;
 
 #-------------------------------------------------------------------------------
 # Setup application config file
 #
 my $cfm = $app->get_app_object('ConfigManager');
 $cfm->select_config_object('defaultConfigObject');
+$cfm->requestFile('buildBuilder');
 $cfm->load;
-$cfm->set_documents([]) unless $cfm->nbr_documents;
-#$log->show_on_warning(1);
+if( $cfm->nbr_documents == 0 )
+{
+  $cfm->set_documents([{}]);
+  $cfm->save;
+}
 
 #-------------------------------------------------------------------------------
 #
 my @args = $cmd->get_arguments;
 if( @args )
 {
-  $self->sayit( "Creating new distribution environments", $self->C_INFO);
+  $self->sayit( "\nCreating new distribution environments", $self->C_INFO);
 
   foreach my $distro_name (@args)
   {
@@ -214,12 +254,29 @@ sub createNewDistro
 
   $self->sayit( "Creating distribution $distro_name", $self->C_INFO);
 
+  my $date = DateTime->now(time_zone => 'Europe/Amsterdam');
+
   my $app = AppState->instance;
   my $cmd = $app->get_app_object('CommandLine');
   my $log = $app->get_app_object('Log');
   my $cfm = $app->get_app_object('ConfigManager');
+  
+  my $distro_path = $distro_name;
+  $distro_path =~ s/::/\//g;
+  $distro_path .= '.pm';
 
-  if( -d $distro_name )
+  my $distro_dir = $distro_name;
+  $distro_dir =~ s/::/-/g;
+
+  my $module_path = "$distro_dir/lib/$distro_path";
+  my $module_dir = "lib/$distro_path";
+  $module_dir =~ s@/[^/]+$@@;
+#  my($module_filename) = $distro_name =~ m/::(;
+  
+say "DN: $distro_name, $distro_path, $distro_dir, $module_path, $module_dir";
+#$self->leave;
+
+  if( -d $distro_dir )
   {
     $self->_log( "Directory $distro_name exists", $self->C_DISTRODIREXISTS);
   }
@@ -228,24 +285,95 @@ sub createNewDistro
   {
     # Create directories
     #
-    File::Path::make_path( $distro_name, {mode => oct(760)});
-    chdir($distro_name);
+    File::Path::make_path( $distro_dir, {mode => oct(755)});
+#    chdir($distro_name);
 
-    File::Path::make_path( 'lib', "lib/$distro_name", 't', 'script', 'doc'
-                         , {mode => oct(760)}
+    File::Path::make_path( "$distro_dir/lib", "$distro_dir/$module_dir"
+                         , "$distro_dir/t", "$distro_dir/script"
+                         , "$distro_dir/doc"
+                         , {mode => oct(755)}
                          );
 
     $self->sayit( "Directories created", $self->C_DIRSCREATED);
 
-
+    # Create Project.yml
+    #
     $cfm->add_config_object( 'Project'
                            , { store_type       => 'Yaml'
                              , location         => $cfm->C_CFF_FILEPATH
-                             , requestFile      => 'Project'
+                             , requestFile      => "$distro_dir/Project"
                              }
                            );
 
-    $self->leave unless $self->loadProjectConfig;
+    my $username = getpwuid $REAL_USER_ID;
+    my $dateExample = $date->year . ' ' . $date->month . ' ' . $date->day;
+
+    $cfm->set_documents([{}]);
+    $cfm->select_document(0);
+
+    $cfm->set_value( 'Application/name', $distro_name);
+    $cfm->set_value( 'Application/abstract', '');
+    $cfm->set_value( 'Application/author/name', $username);
+    $cfm->set_value( 'Application/author/email', '');
+    $cfm->set_value( 'Application/copyright', $date->year);
+    $cfm->set_value( 'Application/documentation', ['README']);
+    $cfm->set_value( 'Application/licenses', ['Perl_5']);
+    $cfm->set_value( 'Application/notes', []);
+    $cfm->set_value( 'Application/version', '0.0.1');
+
+    $cfm->set_value( 'Bugs', {});
+    $cfm->set_value( 'Changes'
+                   , { $dateExample => <<EOTXT
+Original version; created by buildBuilder version $VERSION
+EOTXT
+                     }
+                   );
+    $cfm->set_value( 'Cpan/Account', '');
+    $cfm->set_value( 'Git/github/account', '');
+    $cfm->set_value( 'Git/github/repository', '');
+    $cfm->set_value( 'Git/github/git-ignore-list', ["'.*'"]);
+    $cfm->set_value( 'Manifest-skip-list'
+                   , [ "'.*'"
+                     , qw( ^_build ^Build$ ^blib ~$ \.bak$ ^MANIFEST\.SKIP$)
+                     ]
+                   );
+#    $cfm->set_value( 'Manifest'
+#                   , [ qw( README Changes MANIFEST Build.PL t Doc)
+#                     , 'lib/$distro_name'
+#                     ]
+#                   );
+    $cfm->set_value( 'Readme/description', <<EOTXT
+    The README is used to introduce the module and provide instructions on how
+    to install the module, any machine dependencies it may have (for example C
+    compilers and installed libraries) and any other information that should be
+    provided before the module is installed.
+
+    A README file is required for CPAN modules since CPAN extracts the README
+    file from a module distribution so that people browsing the archive can use
+    it get an idea of the modules uses. It is usually a good idea to provide
+    version information here so that people can decide whether fixes for the
+    module are worth downloading.
+EOTXT
+                   );
+    $cfm->set_value( 'Readme/example', <<EOTXT
+    $distro_name [<options>] [<arguments>]
+EOTXT
+                   );
+    $cfm->set_value( 'Tests', {});
+    $cfm->set_value( 'Todo', { $dateExample => 'Think of what to build'});
+
+    $cfm->save;
+    $self->sayit( 'Project.yml generated', $self->C_INFO);
+
+    $self->find_dependencies;
+    $self->find_config_dependencies;
+
+    $self->generate_readme( $cfm, $distro_dir);
+#    $self->generate_changes($cfm);
+#    $self->generate_manifest($cfm);
+#    $self->generate_module($cfm);
+#    $self->generate_program($cfm);
+#    $self->generate_buildpl($cfm);
   }
 
   return;
@@ -263,7 +391,18 @@ sub updateDistro
   my $cfm = $app->get_app_object('ConfigManager');
 
   my $distro_name = '';
-  say "Update $distro_name distribution";
+  $self->sayit( "\nUpdate $distro_name distribution", $self->C_INFO);
+
+  # Read Project.yml
+  #
+  $cfm->add_config_object( 'Project'
+                         , { store_type       => 'Yaml'
+                           , location         => $cfm->C_CFF_FILEPATH
+                           , requestFile      => 'Project'
+                           }
+                         );
+
+  $self->leave unless $self->loadProjectConfig;
 
   return;
 }
@@ -280,6 +419,145 @@ sub sayit
 
   $self->_log( $message, $code);
   say $message if $cmd->get_option('verbose');
+
+  return;
+}
+
+#-------------------------------------------------------------------------------
+#
+sub find_dependencies
+{
+  my( $self) = @_;
+
+  $self->set_dependency( 'AppState' => '0.4.15');
+
+  return;
+}
+
+#-------------------------------------------------------------------------------
+#
+sub find_config_dependencies
+{
+  my( $self) = @_;
+
+  $self->set_conf_dependency( 'Module::Build' => '0.4205');
+  $self->set_conf_dependency( 'Test::More' => '0.98');
+  $self->set_conf_dependency( 'Test::Most' => '0.33');
+
+  return;
+}
+
+#-------------------------------------------------------------------------------
+#
+sub generate_readme
+{
+  my( $self, $cfm, $distro_dir) = @_;
+
+  my $app = AppState->instance;
+  my $log = $app->get_app_object('Log');
+
+  my $author = $cfm->get_value('Application/author/name');
+  my $distro_name = $cfm->get_value('Application/name');
+  my $version = $cfm->get_value('Application/version');
+  my $description = $cfm->get_value('Readme/description');
+  my $example = $cfm->get_value('Readme/example');
+
+  #-----------------------------------------------------------------------------
+  # Open the redme file
+  #
+  open my $F, '>', "$distro_dir/README";
+
+  my $first_line = "$distro_name version $version";
+  
+  #-----------------------------------------------------------------------------
+  # Write the top line about the module and version
+  #
+  say $F $first_line;
+  say $F '=' x length($first_line);
+  
+  #-----------------------------------------------------------------------------
+  # Write the introduction and body
+  #
+  say $F <<EOTXT;
+
+DESCRIPTION
+
+$description
+
+EXAMPLE
+
+$example
+
+EOTXT
+
+  #-----------------------------------------------------------------------------
+  # Write the dependencies
+  #
+  say $F "DEPENDENCIES\n\n  Program and Modules";
+  my($perlv) = $PERL_VERSION =~ m/^.(.*)$/;
+  say $F sprintf( "    %-40s %-10s", 'perl', $perlv);
+  foreach my $dep ($self->get_dep_keys)
+  {
+    say $F sprintf( "    %-40s %-10s", $dep, $self->get_dependency($dep));
+  }
+
+  say $F "\n  Installation and testing";
+  foreach my $dep ($self->get_conf_dep_keys)
+  {
+    say $F sprintf( "    %-40s %-10s", $dep, $self->get_conf_dependency($dep));
+  }
+
+  #-----------------------------------------------------------------------------
+  # Write the installation method
+  #
+  say $F <<EOTXT;
+
+INSTALLATION
+
+To install this module type the following:
+
+        perl Build.PL
+        Build
+        Build test
+        Build install
+
+EOTXT
+
+  #-----------------------------------------------------------------------------
+  # Write the copyright and license
+  #
+  say $F "COPYRIGHT AND LICENCE\n\n";
+
+  foreach my $license (@{$cfm->get_value('Application/licenses')})
+  {
+    my $license_obj;
+    my $code = <<EOCODE;
+require Software::License::$license;
+\$license_obj = Software::License::$license->new
+                ( { holder => \$author
+                  }
+                );
+EOCODE
+
+    eval($code);
+    my $error = $@;
+    if( $error )
+    {
+      $self->sayit( "Error evaluating code: $error", $self->C_EVALERROR);
+      $self->leave;
+    }
+
+    say $F $license_obj->notice;
+    say $F $license_obj->url // '';
+  }
+
+  #-----------------------------------------------------------------------------
+  # Close
+  #
+  say $F "\n";
+
+  close $F;
+  $self->sayit( 'README generated', $self->C_INFO);
 
   return;
 }
