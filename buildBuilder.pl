13f1df9283f2dda95fb5588c7baf2bc29ce19d83
Marcel Timmerman <mt1957@gmail.com>
2014-03-20 12:50:05 +0100

    generate script and module

--- buildBuilder.pl
+++ buildBuilder.pl
@@ -56,8 +56,11 @@ has options =>
     , default                   =>
       sub
       { return
-        [ [ 'help|h'            => 'Help on this program']
-        , [ 'verbose|v+'        => 'Show all tests done by prove']
+        [ [ 'appstate|A'        => 'Use AppState, implicates the use of Moose']
+        , [ 'help|h'            => 'Help on this program']
+        , [ 'moose|M'           => 'Use Moose in modules']
+        , [ 'verbose|v+'        => 'Show info, errors and warning depending repeats']
+        , [ 'version|V=s'       => 'Minimal perl version']
         ]
       }
     , init_arg                  => undef
@@ -162,9 +165,9 @@ $cmd->initialize( $self->description, $self->arguments, $self->options
                 );
 #say (join ', ', map {"$_=" . $cmd->getOption($_)} sort $cmd->get_options);
 
-# Noverbose or quiet will make proving less noisy
+# Use of AppState implicates use of Moose
 #
-#$cmd->set_option(verbose => 0) if $cmd->getOption('quiet');
+$cmd->set_option(moose => 1) if $cmd->get_option('appstate');
 
 if( $log->get_last_error == $cmd->C_CMD_OPTPROCFAIL or $cmd->get_option('help'))
 {
@@ -193,7 +196,7 @@ if( $cfm->nbr_documents == 0 )
 my @args = $cmd->get_arguments;
 if( @args )
 {
-  $self->sayit( "\nCreating new distribution environments", $self->C_INFO);
+#  $self->sayit( "\nCreating new distribution environments", $self->C_INFO);
 
   foreach my $distro_name (@args)
   {
@@ -252,7 +255,7 @@ sub createNewDistro
 {
   my( $self, $distro_name) = @_;
 
-  $self->sayit( "Creating distribution $distro_name", $self->C_INFO);
+  $self->sayit( "\nCreating distribution $distro_name", $self->C_INFO);
 
   my $date = DateTime->now(time_zone => 'Europe/Amsterdam');
 
@@ -260,6 +263,10 @@ sub createNewDistro
   my $cmd = $app->get_app_object('CommandLine');
   my $log = $app->get_app_object('Log');
   my $cfm = $app->get_app_object('ConfigManager');
+  
+  my $perl_version = $cmd->get_option('version') // $PERL_VERSION;
+  my $use_moose = $cmd->get_option('moose') // '';
+  my $use_appstate = $cmd->get_option('appstate') // '';
   
   my $distro_path = $distro_name;
   $distro_path =~ s/::/\//g;
@@ -273,7 +280,7 @@ sub createNewDistro
   $module_dir =~ s@/[^/]+$@@;
 #  my($module_filename) = $distro_name =~ m/::(;
   
-say "DN: $distro_name, $distro_path, $distro_dir, $module_path, $module_dir";
+#say "DN: $distro_name, $distro_path, $distro_dir, $module_path, $module_dir";
 #$self->leave;
 
   if( -d $distro_dir )
@@ -320,8 +327,11 @@ say "DN: $distro_name, $distro_path, $distro_dir, $module_path, $module_dir";
     $cfm->set_value( 'Application/licenses', ['Perl_5']);
     $cfm->set_value( 'Application/notes', []);
     $cfm->set_value( 'Application/version', '0.0.1');
-
-    $cfm->set_value( 'Bugs', {});
+    $cfm->set_value( 'Application/perl-version', $perl_version);
+    $cfm->set_value( 'Application/use_moose', $use_moose);
+    $cfm->set_value( 'Application/use_appstate', $use_appstate);
+    $cfm->set_value( 'Bugs/dependencies', {});
+    $cfm->set_value( 'Bugs/install-test', {});
     $cfm->set_value( 'Changes'
                    , { $dateExample => <<EOTXT
 Original version; created by buildBuilder version $VERSION
@@ -356,14 +366,24 @@ EOTXT
 EOTXT
                    );
     $cfm->set_value( 'Readme/example', <<EOTXT
-    $distro_name [<options>] [<arguments>]
+    use $distro_name;
+    my \$obj = $distro_name->new(...);
 EOTXT
                    );
-    $cfm->set_value( 'Tests', {});
+    $cfm->set_value( 'Tests'
+                   , [ { module => $distro_name
+                       , 
+                       }
+                     ]
+                   );
     $cfm->set_value( 'Todo', { $dateExample => 'Think of what to build'});
 
     $cfm->save;
     $self->sayit( 'Project.yml generated', $self->C_INFO);
+
+    $self->generate_module( $cfm, $distro_name, $module_path);
+    $self->generate_program( $cfm, $distro_name, $distro_dir);
+    $self->generate_test_program( $cfm, $distro_name, $distro_dir);
 
     $self->find_dependencies;
     $self->find_config_dependencies;
@@ -424,6 +444,513 @@ sub sayit
 }
 
 #-------------------------------------------------------------------------------
+# Generate the distribution module
+#
+sub generate_module
+{
+  my( $self, $cfm, $distro_name, $module_path) = @_;
+
+  my $app = AppState->instance;
+  my $cmd = $app->get_app_object('CommandLine');
+
+  my $module_version = $cfm->get_value('Application/version');
+  my $perl_version = $cfm->get_value('Application/perl-version');
+  my $use_moose = $cfm->get_value('Application/use_moose');
+  my $use_appstate = $cfm->get_value('Application/use_appstate');
+
+  #-----------------------------------------------------------------------------
+  # Open the module file
+  #
+  open my $F, '>', "$module_path";
+
+  #-----------------------------------------------------------------------------
+  # Write 
+  #
+  print $F <<EOCODE;
+package $distro_name;
+
+use Modern::Perl;
+use version; our \$VERSION = qv('v$module_version');
+use $perl_version;
+
+use namespace::autoclean;
+EOCODE
+
+  #-----------------------------------------------------------------------------
+  # If AppState option is set, then the module is setup using AppState and Moose
+  #
+  if( $use_appstate )
+  {
+    print $F <<EOCODE;    
+use Moose;
+extends qw(AppState::Ext::Constants);
+
+use AppState;
+
+...
+
+sub BUILD
+{
+  my(\$self) = \@_;
+
+  if( \$self->meta->is_mutable )
+  {
+    \$self->code_reset;
+#    \$self->const( 'C_SOMECONST0', qw( M_F_INFO M_SUCCESS));
+#    \$self->const( 'C_SOMECONST1', qw( M_F_ERROR M_FAIL));
+#    \$self->const( 'C_SOMECONST2', qw( M_WARNING M_FORCED));
+
+    __PACKAGE__->meta->make_immutable;
+  }
+}
+
+...
+
+# End of package
+#
+1;
+
+EOCODE
+  }
+  
+  #-----------------------------------------------------------------------------
+  # If Moose option is set, then the module is setup using Moose only
+  #
+  elsif( $use_moose )
+  {
+    print $F <<EOCODE;    
+use Moose;
+
+...
+
+# End of package
+#
+__PACKAGE__->meta->make_immutable;
+1;
+
+EOCODE
+  }
+  
+  #-----------------------------------------------------------------------------
+  # If no Moose option is set, then the module is setup as a plain class
+  #
+  else
+  {
+    print $F <<EOCODE;    
+
+# Constructor
+#
+sub new
+{
+  my( \$class, \%options) = \@_;
+  
+  my \$self = {\%options};
+
+  ...
+  
+  return bless \$self, \$class;
+}
+
+...
+
+# End of package
+#
+1;
+
+EOCODE
+  }
+
+  #-----------------------------------------------------------------------------
+  # Close
+  #
+  print $F "\n";
+
+  close $F;
+  $self->sayit( "$module_path generated", $self->C_INFO);
+
+  return;
+}
+
+#-------------------------------------------------------------------------------
+# Generate the distribution program
+#
+sub generate_program
+{
+  my( $self, $cfm, $distro_name, $distro_dir) = @_;
+
+  my $app = AppState->instance;
+  my $cmd = $app->get_app_object('CommandLine');
+
+  my $module_version = $cfm->get_value('Application/version');
+  my $perl_version = $cfm->get_value('Application/perl-version');
+  my $use_moose = $cfm->get_value('Application/use_moose');
+  my $use_appstate = $cfm->get_value('Application/use_appstate');
+
+  #-----------------------------------------------------------------------------
+  # Open the module file
+  #
+  open my $F, '>', "$distro_dir/script/program.pl";
+
+  #-----------------------------------------------------------------------------
+  # Write 
+  #
+  print $F <<EOCODE;
+#!/usr/bin/env perl
+#
+use Modern::Perl;
+use version; our \$VERSION = qv('v$module_version');
+use $perl_version;
+
+use $distro_name;
+
+# What I found very usefull is to setup the main as if it where a module so
+# it can be instantiated, inherit other modules and so forth.
+
+EOCODE
+
+  #-----------------------------------------------------------------------------
+  # If AppState option is set, then the module is setup using AppState and Moose
+  #
+  if( $use_appstate )
+  {
+    print $F <<EOCODE;    
+use Moose;
+extends qw(AppState::Ext::Constants);
+
+use AppState;
+
+...
+
+sub BUILD
+{
+  my(\$self) = \@_;
+
+  if( \$self->meta->is_mutable )
+  {
+    \$self->code_reset;
+#    \$self->const( 'C_SOMECONST0', qw( M_F_INFO M_SUCCESS));
+#    \$self->const( 'C_SOMECONST1', qw( M_F_ERROR M_FAIL));
+#    \$self->const( 'C_SOMECONST2', qw( M_WARNING M_FORCED));
+
+    __PACKAGE__->meta->make_immutable;
+  }
+}
+
+# Make object from the main class
+#
+my \$self = main->new(...);
+
+...
+
+# End of program
+#
+\$self->leave;
+
+# Destructor
+#
+sub DEMOLISH
+{
+  my(\$self) = \@_;
+  
+  ...
+}
+
+# Subroutines
+#
+...
+
+EOCODE
+  }
+  
+  #-----------------------------------------------------------------------------
+  # If Moose option is set, then the module is setup using Moose only
+  #
+  elsif( $use_moose )
+  {
+    print $F <<EOCODE;    
+use Moose;
+
+...
+
+__PACKAGE__->meta->make_immutable;
+
+# Make object from the main class
+#
+my \$self = main->new(...);
+
+...
+
+# End of program
+#
+\$self->DESTROY;
+exit(0);
+
+# Destructor
+#
+sub DEMOLISH
+{
+  my(\$self) = \@_;
+  
+  ...
+}
+
+# Subroutines
+#
+...
+
+EOCODE
+  }
+  
+  #-----------------------------------------------------------------------------
+  # If no Moose option is set, then the module is setup as a plain class
+  #
+  else
+  {
+    print $F <<EOCODE;    
+...
+
+# Make object from the main class
+#
+my \$self = main->new(...);
+
+...
+
+# End of program
+#
+\$self->DESTROY;
+exit(0);
+
+# Constructor
+#
+sub new
+{
+  my( \$class, \%options) = \@_;
+  
+  my \$self = {\%options};
+
+  ...
+  
+  return bless \$self, \$class;
+}
+
+# Destructor
+#
+sub DESTROY
+{
+  my(\$self) = \@_;
+  
+  ...
+}
+
+# Subroutines
+#
+...
+
+EOCODE
+  }
+
+  #-----------------------------------------------------------------------------
+  # Close
+  #
+  print $F "\n";
+
+  close $F;
+  $self->sayit( "$distro_dir/script/program.pl generated", $self->C_INFO);
+
+  return;
+}
+
+
+#-------------------------------------------------------------------------------
+# Generate the test program
+#
+sub generate_test_program
+{
+  my( $self, $cfm, $distro_name, $distro_dir) = @_;
+
+  my $app = AppState->instance;
+  my $cmd = $app->get_app_object('CommandLine');
+
+  my $module_version = $cfm->get_value('Application/version');
+  my $perl_version = $cfm->get_value('Application/perl-version');
+  my $use_moose = $cfm->get_value('Application/use_moose');
+  my $use_appstate = $cfm->get_value('Application/use_appstate');
+
+  #-----------------------------------------------------------------------------
+  # Open the module file
+  #
+  open my $F, '>', "$distro_dir/t/100-test.t";
+
+  #-----------------------------------------------------------------------------
+  # Write 
+  #
+  print $F <<EOCODE;
+#!/usr/bin/env perl
+#
+use Modern::Perl;
+use version; our \$VERSION = qv('v$module_version');
+use $perl_version;
+
+use $distro_name;
+
+# What I found very usefull is to setup the main as if it where a module so
+# it can be instantiated, inherit other modules and so forth.
+
+EOCODE
+
+  #-----------------------------------------------------------------------------
+  # If AppState option is set, then the module is setup using AppState and Moose
+  #
+  if( $use_appstate )
+  {
+    print $F <<EOCODE;    
+use Moose;
+extends qw(AppState::Ext::Constants);
+
+use AppState;
+
+...
+
+sub BUILD
+{
+  my(\$self) = \@_;
+
+  if( \$self->meta->is_mutable )
+  {
+    \$self->code_reset;
+#    \$self->const( 'C_SOMECONST0', qw( M_F_INFO M_SUCCESS));
+#    \$self->const( 'C_SOMECONST1', qw( M_F_ERROR M_FAIL));
+#    \$self->const( 'C_SOMECONST2', qw( M_WARNING M_FORCED));
+
+    __PACKAGE__->meta->make_immutable;
+  }
+}
+
+# Make object from the main class
+#
+my \$self = main->new(...);
+
+...
+
+# End of program
+#
+\$self->leave;
+
+# Destructor
+#
+sub DEMOLISH
+{
+  my(\$self) = \@_;
+  
+  ...
+}
+
+# Subroutines
+#
+...
+
+EOCODE
+  }
+  
+  #-----------------------------------------------------------------------------
+  # If Moose option is set, then the module is setup using Moose only
+  #
+  elsif( $use_moose )
+  {
+    print $F <<EOCODE;    
+use Moose;
+
+...
+
+__PACKAGE__->meta->make_immutable;
+
+# Make object from the main class
+#
+my \$self = main->new(...);
+
+...
+
+# End of program
+#
+\$self->DESTROY;
+exit(0);
+
+# Destructor
+#
+sub DEMOLISH
+{
+  my(\$self) = \@_;
+  
+  ...
+}
+
+# Subroutines
+#
+...
+
+EOCODE
+  }
+  
+  #-----------------------------------------------------------------------------
+  # If no Moose option is set, then the module is setup as a plain class
+  #
+  else
+  {
+    print $F <<EOCODE;    
+...
+
+# Make object from the main class
+#
+my \$self = main->new(...);
+
+...
+
+# End of program
+#
+\$self->DESTROY;
+exit(0);
+
+# Constructor
+#
+sub new
+{
+  my( \$class, \%options) = \@_;
+  
+  my \$self = {\%options};
+
+  ...
+  
+  return bless \$self, \$class;
+}
+
+# Destructor
+#
+sub DESTROY
+{
+  my(\$self) = \@_;
+  
+  ...
+}
+
+# Subroutines
+#
+...
+
+EOCODE
+  }
+
+  #-----------------------------------------------------------------------------
+  # Close
+  #
+  print $F "\n";
+
+  close $F;
+  $self->sayit( "$distro_dir/script/program.pl generated", $self->C_INFO);
+
+  return;
+}
+
+#-------------------------------------------------------------------------------
 #
 sub find_dependencies
 {
@@ -463,7 +990,7 @@ sub generate_readme
   my $example = $cfm->get_value('Readme/example');
 
   #-----------------------------------------------------------------------------
-  # Open the redme file
+  # Open the readme file
   #
   open my $F, '>', "$distro_dir/README";
 
